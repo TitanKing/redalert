a438a408e1a4dd4de07b7f42e6e1ce22a1e2d141
TitanKing <titan@phpdevshell.org>
2015-03-17 16:57:18 +0200

    Testing digital object.

--- redalert.py
+++ redalert.py
@@ -4,11 +4,9 @@ from time import sleep
 
 while True:
     sleep(1)
+    pfd.leds[0].toggle()
+    sleep(1)
     pfd.leds[1].toggle()
-    sleep(1)
-    pfd.leds[2].toggle()
-    sleep(1)
-    pfd.leds[3].toggle()
 
 # pfio.init()
 # from time import sleep
