3b9c099caf981f93381ccd1f5025c9f46849d9e3
TitanKing <titan@phpdevshell.org>
2015-03-17 16:54:35 +0200

    Testing digital object.

--- redalert.py
+++ redalert.py
@@ -1,11 +1,13 @@
 import pifacedigitalio as pfio
 pfd = pfio.PiFaceDigital()
 
-pfio.init()
-from time import sleep
+pfd.leds[2].toggle()
 
-while True:
-        pfio.digital_write(0, 1)
-        sleep(1)
-        pfio.digital_write(0, 0)
-        sleep(1)
+# pfio.init()
+# from time import sleep
+
+# while True:
+#        pfio.digital_write(0, 1)
+#        sleep(1)
+#        pfio.digital_write(0, 0)
+#        sleep(1)
