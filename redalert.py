50ea59f37a3a49d3ad97b70cbf1dc08738b14de0
TitanKing <titan@phpdevshell.org>
2015-03-20 17:46:45 +0200

    Testing digital object.

--- redalert.py
+++ redalert.py
@@ -3,13 +3,16 @@ import subprocess
 pfd = pfio.PiFaceDigital()
 from time import sleep
 
-subprocess.check_output('echo "Red alert security system started" | festival --tts', shell=True)
+subprocess.check_output('123" | festival --tts', shell=True)
 
-while True:
-    sleep(1)
-    pfd.leds[0].toggle()
-    sleep(1)
-    pfd.leds[1].toggle()
+pfd.output_pins[0].value = 1
+
+
+# while True:
+#    sleep(1)
+#    pfd.leds[0].toggle()
+#    sleep(1)
+#    pfd.leds[1].toggle()
 
 # pfio.init()
 # from time import sleep
@@ -19,3 +22,4 @@ while True:
 #        sleep(1)
 #        pfio.digital_write(0, 0)
 #        sleep(1)
+
